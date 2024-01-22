ad9eecda37c1309d9096ef74f68bf7d388a48029
LokeshR29 <RLokesh@stellaripl.com>
2024-01-22 11:19:26 +0530

    Changes

--- Iphone _ Task1/resources/views/welcome.blade.php
+++ Iphone _ Task1/resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
             </ul>
           </div>
           <div class="col-md-3 text-end ">
-            <button type="button" class="btn btn-info btn-dark me-2" >Login</button>
+            <button type="button" class="btn btn-info btn-dark me-2" id="LogIn">Login</button>
             <button type="button" class="btn btn-info me-2 btn-dark" id="SignIn">Sign Up</button>
             <button type="button" class="btn btn-info me-2 btn-dark"><a href="/checkout">Purchase </a></button>
           </div>
@@ -309,6 +309,48 @@
         </div>
     </div>
 
+    <!-- Modal -->
+
+    <div class="modal modal-sheet bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalLogin">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content rounded-4 shadow">
+            <div class="modal-header p-5 pb-4 border-bottom-0">
+              <h1 class="fw-bold mb-0 fs-2">Log In</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body p-5 pt-0">
+              <form class="">
+                <div class="form-floating mb-3">
+                  <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
+                  <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
+                  <label for="floatingPassword">Password</label>
+                </div>
+                <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Log In</button>
+
+                <hr class="my-4">
+                <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
+                <button class="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
+                  <img src="{{asset('img/twitter-square.svg')}}" class="bi me-1" width="18" height="18">
+                  Continue with Twitter
+                </button>
+                <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-3" type="submit">
+                    <img src="{{asset('img/facebook.svg')}}" class="bi me-1" width="18" height="18">
+                  Continue with Facebook
+                </button>
+                <button class="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
+                  <img src="{{asset('img/github-square.svg')}}" class="bi me-1" width="18" height="18">
+                  Continue with GitHub
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+
     <!-- Javscript-->
 
     <script>
@@ -316,6 +358,12 @@
             $('#SignIn').click(function(){
                 $('#modalSignin').modal('show');
             });
+
+
+            $('#LogIn').click(function(){
+                $('#modalLogin').modal('show');
+            });
+
         });
     </script>
 
